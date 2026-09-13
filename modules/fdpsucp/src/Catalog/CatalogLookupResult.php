4815743589e5a294dd738fd68437779112fcbe92
Madmin27 <evpc77@gmail.com>
2026-09-13 12:37:38 +0300

    compat: avoid PHP 8.1 readonly requirement

--- modules/fdpsucp/src/Catalog/CatalogLookupResult.php
+++ modules/fdpsucp/src/Catalog/CatalogLookupResult.php
@@ -8,8 +8,12 @@ if (!defined('_PS_VERSION_')) {
 
 final class CatalogLookupResult
 {
+    /** @var array<int,array<string,mixed>> */
+    public array $products;
+
     /** @param array<int,array<string,mixed>> $products */
-    public function __construct(public readonly array $products)
+    public function __construct(array $products)
     {
+        $this->products = $products;
     }
 }
