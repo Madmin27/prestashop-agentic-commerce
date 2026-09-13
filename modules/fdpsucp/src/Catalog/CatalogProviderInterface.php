973d8689395e124ce343510e9074389b40e5e1a8
Madmin27 <evpc77@gmail.com>
2026-09-13 22:34:27 +0300

    feat(ucp): allow lookup filters and context in catalog providers

--- modules/fdpsucp/src/Catalog/CatalogProviderInterface.php
+++ modules/fdpsucp/src/Catalog/CatalogProviderInterface.php
@@ -1,8 +1,18 @@
 <?php
+
 namespace FD\PrismUcp\Catalog;
-if (!defined('_PS_VERSION_')) { exit; }
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
 interface CatalogProviderInterface
 {
     public function search(array $params): CatalogSearchResult;
-    public function lookup(array $ids): CatalogLookupResult;
+
+    /**
+     * @param array<int,string> $ids
+     * @param array<string,mixed> $params Optional lookup filters/context.
+     */
+    public function lookup(array $ids, array $params = []): CatalogLookupResult;
 }
