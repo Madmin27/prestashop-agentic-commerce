c9fda3a24154544cc4b99c85d29b34bc5c11eb8e
Madmin27 <evpc77@gmail.com>
2026-09-13 12:39:48 +0300

    test: cover object-only metadata validation

--- modules/psagenticcommerce/tests/unit/JsonPayloadValidatorTest.php
+++ modules/psagenticcommerce/tests/unit/JsonPayloadValidatorTest.php
@@ -1,9 +1,28 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 use PrestaShopAgenticCommerce\Validation\JsonPayloadValidator;
-final class JsonPayloadValidatorTest extends TestCase {
-  public function testRoundTrip(): void {
-    $v=['x'=>1];
-    self::assertSame($v, JsonPayloadValidator::decodeNullable(JsonPayloadValidator::encodeNullable($v)));
-  }
+
+final class JsonPayloadValidatorTest extends TestCase
+{
+    public function testRoundTrip(): void
+    {
+        $value = ['x' => 1];
+        self::assertSame(
+            $value,
+            JsonPayloadValidator::decodeNullable(JsonPayloadValidator::encodeNullable($value))
+        );
+    }
+
+    public function testObjectDecoderRejectsListPayload(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        JsonPayloadValidator::decodeObjectNullable('["x"]');
+    }
+
+    public function testObjectDecoderRejectsMalformedPayload(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        JsonPayloadValidator::decodeObjectNullable('{bad json}');
+    }
 }
