e86cd563dd38876f2b2f5c00cbf47204540af717
Madmin27 <evpc77@gmail.com>
2026-09-13 12:37:32 +0300

    compat: avoid PHP 8.1 readonly requirement

--- modules/fdpsucp/src/Catalog/CatalogSearchResult.php
+++ modules/fdpsucp/src/Catalog/CatalogSearchResult.php
@@ -8,11 +8,16 @@ if (!defined('_PS_VERSION_')) {
 
 final class CatalogSearchResult
 {
+    /** @var array<int,array<string,mixed>> */
+    public array $products;
+    public int $totalCount;
+    public bool $hasNextPage;
+
     /** @param array<int,array<string,mixed>> $products */
-    public function __construct(
-        public readonly array $products,
-        public readonly int $totalCount,
-        public readonly bool $hasNextPage
-    ) {
+    public function __construct(array $products, int $totalCount, bool $hasNextPage)
+    {
+        $this->products = $products;
+        $this->totalCount = $totalCount;
+        $this->hasNextPage = $hasNextPage;
     }
 }
